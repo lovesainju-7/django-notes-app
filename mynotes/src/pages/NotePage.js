071cb077b92211a42f22ef432b08f2c507a495a3
lovesainju <sainju.love69@gmail.com>
2026-02-01 15:20:44 +0545

    basic cahnge

--- mynotes/src/pages/NotePage.js
+++ mynotes/src/pages/NotePage.js
@@ -1,81 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import { useParams,useNavigate,Link } from 'react-router-dom'
-// import notes from '../assets/data.js'
-import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = () => {
-  let { id }  = useParams();
-  let navigate = useNavigate()
-  let [note, setNote] = useState({})
+  let { id } = useParams();
+  let navigate = useNavigate();
+  let [note, setNote] = useState({});
+
   useEffect(() => {
     let getNote = async () => {
-      if (id === 'new') return
-      let response = await fetch(`/api/notes/${id}`)
-      let data = await response.json()
-      console.log(data)
-      setNote(data)
-    }
-    getNote()
-  }, [id])
+      if (id === "new") return;
+      let response = await fetch(`/api/notes/${id}`);
+      let data = await response.json();
+      setNote(data);
+    };
+    getNote();
+  }, [id]);
 
   let createNote = async () => {
     await fetch(`/api/notes/create/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({...note, 'updated': new Date()})
-    })
-  }
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
 
   let updateNote = async () => {
     await fetch(`/api/notes/${id}/update/`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({...note, 'updated': new Date()})
-    })
-  }
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
 
   let deleteNote = async () => {
-    await fetch(`/api/notes/${id}/delete/`, {
-      method: 'DELETE'
-    })
-    navigate('/')
-  }
+    await fetch(`/api/notes/${id}/delete/`, { method: "DELETE" });
+    navigate("/");
+  };
 
   let handleSubmit = () => {
-    if (id !== 'new' && !note.body) {
-      deleteNote()
-    } else if (id !== 'new') {
-      updateNote()
-    } else if (id ==='new' && note !== null) {
-      createNote()
+    if (id !== "new" && !note.body) {
+      deleteNote();
+    } else if (id !== "new") {
+      updateNote();
+    } else if (id === "new" && note.body) {
+      createNote();
     }
-    navigate('/')
-  }
+    navigate("/");
+  };
 
   return (
-    <div className='note'>
+    <div className="note">
       <div className="note-header">
+        {/* LEFT BACK ARROW */}
         <h3>
-          <Link to='/'>
+          <Link to="/">
             <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
-        {id !== 'new' ? (
-          <button onClick={deleteNote}>Delete</button>
-        ):(
-          <button onClick={handleSubmit}>Save</button>
-        )}
+
+        {/* RIGHT SIDE BUTTONS */}
+        <div className="note-header-right">
+          <Link to="/">
+            <button className="list-note-btn">LIST NOTE</button>
+          </Link>
+
+          {id !== "new" ? (
+            <button onClick={deleteNote}>Delete</button>
+          ) : (
+            <button onClick={handleSubmit}>Save</button>
+          )}
+        </div>
       </div>
+
       <div className="note-body">
-        <textarea onChange={(e) => {setNote({...note, 'body':e.target.value})}} value={note.body}>  
-        </textarea>
+        <textarea
+          onChange={(e) => setNote({ ...note, body: e.target.value })}
+          value={note.body || ""}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotePage
+export default NotePage;
